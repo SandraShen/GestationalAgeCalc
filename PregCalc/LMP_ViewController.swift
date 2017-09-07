6c10942039707799ae315972e91b77795cfa9e93
shen.yingyin <shen.yingyin@i3design.co.jp>
2017-09-07 17:02:28 +0900

    develop

---
+++ PregCalc/LMP_ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LMP_ViewController.swift
+//  PregCalc
+//
+//  Created by 折戸　征也 on 12/14/15.
+//  Copyright © 2015 折戸医院. All rights reserved.
+//
+
+import UIKit
+
+class LMP_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
